b23c4ccb553b7ba1f535cd3befed571e4a4e9d93
ZubairAnwer <zubairmohomad4@gmail.com>
2024-10-20 12:59:08 +0500

    updated

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -6,7 +6,7 @@ function Items(){
 
         <div className="flex justify-around">
         <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/1.jpg" alt="wall decor" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/1.jpg" alt="wall decor" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Black Glass Art with Copper Edging</h3>
       <p className="text-gray-600">$12.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -16,7 +16,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/2.jpg" alt="wall planter" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/2.jpg" alt="wall planter" width={200} height={250}/>
       <h3 className="text-xl font-semibold">6pcs Vibrant Wooden Wall Planter</h3>
       <p className="text-gray-600">$9.99</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -26,7 +26,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/3.jpg" alt="square wall piece" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/3.jpg" alt="square wall piece" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Elegant Square Wall Piece</h3>
       <p className="text-gray-600">$10.99</p>
       <span className="text-yellow-500">★★★☆☆</span>
@@ -38,7 +38,7 @@ function Items(){
 
     <div className="flex justify-around">
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/4.jpg" alt="lightened wall accent" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/4.jpg" alt="lightened wall accent" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Light-Up Classy Wall Accent</h3>
       <p className="text-gray-600">$22.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -48,7 +48,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/5.jpg" alt="chair duo,table & rack" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/5.jpg" alt="chair duo,table & rack" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Chair Duo with Table & Wall Rack</h3>
       <p className="text-gray-600">$25.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -58,7 +58,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/6.jpg" alt="lead & birds" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/6.jpg" alt="lead & birds" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Leaf & Birds Wall Set 8pcs</h3>
       <p className="text-gray-600">$18.99</p>
       <span className="text-yellow-500">★★☆☆☆</span>
@@ -70,7 +70,7 @@ function Items(){
 
         <div className="flex justify-around">
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/7.jpg" alt="light frame" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/7.jpg" alt="light frame" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Sleek Noir Hanging frame Lights</h3>
       <p className="text-gray-600">$25.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -80,7 +80,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/8.jpg" alt="Elegant Dreamscape Fixtures" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/8.jpg" alt="Elegant Dreamscape Fixtures" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Elegant Dreamscape Fixtures</h3>
       <p className="text-gray-600">$16.00</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -90,7 +90,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/9.jpg" alt="moonlight stand" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/9.jpg" alt="moonlight stand" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Moonlight with golden iron Stand</h3>
       <p className="text-gray-600">$14.00</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -102,7 +102,7 @@ function Items(){
 
         <div className="flex justify-around">
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/10.jpg" alt="living room wall piece" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/10.jpg" alt="living room wall piece" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Living Room lightened wall Piece</h3>
       <p className="text-gray-600">$30.00</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -112,7 +112,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/11.jpg" alt="Ethereal Crystal Lantern" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/11.jpg" alt="Ethereal Crystal Lantern" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Ethereal Crystal Lantern</h3>
       <p className="text-gray-600">$26.98</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -122,7 +122,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/12.jpg" alt="Woodland Tree Shelf" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/12.jpg" alt="Woodland Tree Shelf" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Woodland Tree Shelf</h3>
       <p className="text-gray-600">$32.00</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -134,7 +134,7 @@ function Items(){
 
         <div className="flex justify-around">
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/13.jpg" alt="rounded couch" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/13.jpg" alt="rounded couch" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Fluffy Rounded Couch with Cushion</h3>
       <p className="text-gray-600">$40.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -144,7 +144,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/14.jpg" alt="couch & shelf combo" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/14.jpg" alt="couch & shelf combo" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Lit Haven Couch & Shelf Combo</h3>
       <p className="text-gray-600">$150.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -154,7 +154,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/15.jpg" alt="sofa+cushions and table" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/15.jpg" alt="sofa+cushions and table" width={200} height={250}/>
       <h3 className="text-xl font-semibold">corner Sofa Set With Center Table</h3>
       <p className="text-gray-600">$200</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -166,7 +166,7 @@ function Items(){
 
     <div className="flex justify-around">
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/16.jpg" alt="Funky Nest Seating" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/16.jpg" alt="Funky Nest Seating" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Funky Soft Nest Seating</h3>
       <p className="text-gray-600">$51.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -176,7 +176,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/17.jpg" alt="wall panels" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/17.jpg" alt="wall panels" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Lively ColorS Wall Panels</h3>
       <p className="text-gray-600">$44.99</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -186,7 +186,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/18.jpg" alt="reflections art" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/18.jpg" alt="reflections art" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Living Room Reflections Wall Art</h3>
       <p className="text-gray-600">$99.99</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -198,7 +198,7 @@ function Items(){
 
     <div className="flex justify-around">
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/19.jpg" alt="sofa and twisted lamp" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/19.jpg" alt="sofa and twisted lamp" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Cozy White Sofa with Twisted Lamp</h3>
       <p className="text-gray-600">$69.99</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -208,7 +208,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/20.jpg" alt="comfy sofa" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/20.jpg" alt="comfy sofa" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Comfortable dual color</h3>
       <p className="text-gray-600">$84.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -218,7 +218,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/21.jpg" alt="indoor swings" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/21.jpg" alt="indoor swings" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Indoor Swing with Soft Pillows</h3>
       <p className="text-gray-600">$79.99</p>
       <span className="text-yellow-500">★★★★☆</span>
@@ -230,7 +230,7 @@ function Items(){
 
     <div className="flex justify-around">
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/22.jpg" alt="outdoor swing" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/22.jpg" alt="outdoor swing" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Outdoor Swing</h3>
       <p className="text-gray-600">$65.00</p>
       <span className="text-yellow-500">★★★☆☆</span>
@@ -240,7 +240,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/23.jpg" alt="garden Area swings" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/23.jpg" alt="garden Area swings" width={200} height={250}/>
       <h3 className="text-xl font-semibold">3 Garden Area Swings, mini Table</h3>
       <p className="text-gray-600">$299.99</p>
       <span className="text-yellow-500">★★★★★</span>
@@ -250,7 +250,7 @@ function Items(){
     </div>
 
     <div className="pl-10 mb-4">
-      <Image className="h-72 w-80 mt-28" src="/images/24.jpg" alt="elegant swing" width={80} height={72}/>
+      <Image className="h-72 w-80 mt-28" src="/images/24.jpg" alt="elegant swing" width={200} height={250}/>
       <h3 className="text-xl font-semibold">Glass Swing with golden Stand</h3>
       <p className="text-gray-600">$169.99</p>
       <span className="text-yellow-500">★★★☆☆</span>
--- src/components/Producs.tsx
+++ src/components/Producs.tsx
@@ -7,19 +7,19 @@ function Products(){
           <div className="text-4xl text-black font-bold pt-11 ml-[500px]">Best Selling Products</div>
           {/* products */}
           <div className="flex justify-around">
-        <Image className="mt-28 w-96 h-80" src="/images/pic1.jpg" alt="abc" width={80} height={72}/>
-        <Image className="mt-28 w-96 h-80" src="/images/pic2.jpg" alt="abc"  width={80} height={72}/>
-        <Image className="mt-28 w-96 h-80" src="/images/pic3.jpg" alt="abc"  width={80} height={72}/>
+        <Image className="mt-28 w-96 h-80" src="/images/pic1.jpg" alt="abc" width={300} height={350}/>
+        <Image className="mt-28 w-96 h-80" src="/images/pic2.jpg" alt="abc"  width={300} height={350}/>
+        <Image className="mt-28 w-96 h-80" src="/images/pic3.jpg" alt="abc"  width={300} height={350}/>
         </div>
         <div className="flex justify-around">
-        <Image className="mt-10 w-96 h-80" src="/images/pic4.jpg" alt="abc"  width={80} height={72}/>
-        <Image className="mt-10 w-96 h-80" src="/images/pic5.jpg" alt="abc"  width={80} height={72}/>
-        <Image className="mt-10 w-96 h-80" src="/images/pic6.jpg" alt="abc"  width={80} height={72}/>
+        <Image className="mt-10 w-96 h-80" src="/images/pic4.jpg" alt="abc"  width={300} height={350}/>
+        <Image className="mt-10 w-96 h-80" src="/images/pic5.jpg" alt="abc"  width={300} height={350}/>
+        <Image className="mt-10 w-96 h-80" src="/images/pic6.jpg" alt="abc"  width={300} height={350}/>
         </div>
         <div className="flex justify-around mb-20">
-        <Image className="mt-10 w-96 h-80" src="/images/pic7.jpg" alt="abc" width={80} height={50}/>
-        <Image className="mt-10 w-96 h-80" src="/images/pic8.jpg" alt="abc" width={80} height={50}/>
-        <Image className="mt-10 w-96 h-80" src="/images/pic9.jpg" alt="abc" width={80} height={50}/>
+        <Image className="mt-10 w-96 h-80" src="/images/pic7.jpg" alt="abc" width={300} height={300}/>
+        <Image className="mt-10 w-96 h-80" src="/images/pic8.jpg" alt="abc" width={250} height={300}/>
+        <Image className="mt-10 w-96 h-80" src="/images/pic9.jpg" alt="abc" width={250} height={300}/>
         </div>
         </main>
     </>
